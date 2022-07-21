2974bd565d3308bb377e4ba85661155959ab2567
amay077 <okuokuoku@gmail.com>
2022-07-21 22:23:53 +0900

    #1 top ページは limit 16, /posts は全件になった

---
+++ src/components/BlogRoll2.js
@@ -0,0 +1,57 @@
+import React from "react";
+import { Link } from "gatsby";
+import dayjs from 'dayjs'
+import timezone from "dayjs/plugin/timezone";
+import utc from "dayjs/plugin/utc";
+
+dayjs.extend(timezone);
+dayjs.extend(utc);
+
+const BlogRoll2 = class extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const { posts } = this.props;
+    return (
+
+      <div className="columns is-multiline">
+      {posts && posts.map(({ node: post }) => (
+        <div className="is-parent column is-6" key={post.id}>
+        <article
+          className={`blog-list-item tile is-child box notification ${
+            post.frontmatter.featuredpost ? 'is-featured' : ''
+          }`}
+        >
+          <header>
+            <p className="post-meta">
+              <Link
+                className="title has-text-primary is-size-4"
+                to={post.fields.slug}
+              >
+                {post.frontmatter.title}
+              </Link>
+              <span></span>
+              <span className="subtitle is-size-5 is-block">
+                {dayjs(post.frontmatter.date).tz('Asia/Tokyo').format('YYYY-MM-DD HH:mm:ss')}
+              </span>
+            </p>
+          </header>
+          <p>
+            {post.excerpt}
+            <br />
+            <br />
+            <Link className="button" to={post.fields.slug}>
+              Keep Reading →
+            </Link>
+          </p>
+        </article>
+        </div>
+      ))}
+      </div>
+    );
+  }
+};
+
+export default BlogRoll2;
--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -5,18 +5,15 @@ import { getImage } from "gatsby-plugin-image";
 
 import Layout from "../components/Layout";
 import Features from "../components/Features";
-import BlogRoll from "../components/BlogRoll";
+import BlogRoll2 from "../components/BlogRoll2";
 import FullWidthImage from "../components/FullWidthImage";
 
 // eslint-disable-next-line
 export const IndexPageTemplate = ({
   image,
   title,
-  heading,
   subheading,
-  mainpitch,
-  description,
-  intro,
+  posts,
 }) => {
   const heroImage = getImage(image) || image;
 
@@ -29,37 +26,11 @@ export const IndexPageTemplate = ({
             <div className="columns">
               <div className="column is-10 is-offset-1">
                 <div className="content">
-                  {/* 
-                  <div className="content">
-                    <div className="tile">
-                      <h1 className="title">{mainpitch.title}</h1>
-                    </div>
-                    <div className="tile">
-                      <h3 className="subtitle">{mainpitch.description}</h3>
-                    </div>
-                  </div>
-                  <div className="columns">
-                    <div className="column is-12">
-                      <h3 className="has-text-weight-semibold is-size-2">
-                        {heading}
-                      </h3>
-                      <p>{description}</p>
-                    </div>
-                  </div>
-                  <Features gridItems={intro.blurbs} />
-                  <div className="columns">
-                    <div className="column is-12 has-text-centered">
-                      <Link className="btn" to="/products">
-                        See all products
-                      </Link>
-                    </div>
-                  </div>
-                  */}
                   <div className="column is-12">
                     <h3 className="has-text-weight-semibold is-size-2">
                       Latest posts
                     </h3>
-                    <BlogRoll />
+                    <BlogRoll2 posts={posts} />
                     <div className="column is-12 has-text-centered">
                       <Link className="btn" to="/posts">
                         Read more
@@ -90,6 +61,7 @@ IndexPageTemplate.propTypes = {
 
 const IndexPage = ({ data }) => {
   const { frontmatter } = data.markdownRemark;
+  const { edges: posts } = data.allMarkdownRemark
 
   return (
     <Layout>
@@ -101,6 +73,7 @@ const IndexPage = ({ data }) => {
         mainpitch={frontmatter.mainpitch}
         description={frontmatter.description}
         intro={frontmatter.intro}
+        posts={posts}
       />
     </Layout>
   );
@@ -118,6 +91,37 @@ export default IndexPage;
 
 export const pageQuery = graphql`
   query IndexPageTemplate {
+    allMarkdownRemark(
+      sort: { order: DESC, fields: [frontmatter___date] }
+      filter: { frontmatter: { templateKey: { eq: "blog-post" } } }
+      limit: 16
+    ) {
+      edges {
+        node {
+          excerpt(pruneLength: 200)
+          id
+          fields {
+            slug
+          }
+          frontmatter {
+            title
+            templateKey
+            date
+            featuredpost
+            featuredimage {
+              childImageSharp {
+                gatsbyImageData(
+                  width: 120
+                  quality: 100
+                  layout: CONSTRAINED
+                )
+
+              }
+            }
+          }
+        }
+      }
+    }    
     markdownRemark(frontmatter: { templateKey: { eq: "index-page" } }) {
       frontmatter {
         title
