d6cb7201487772854c0ae1bc4b84761bef128da5
amay077 <okuokuoku@gmail.com>
2023-03-11 11:18:08 +0900

    remove gatsby-plugin-netlify

--- gatsby-config.js
+++ gatsby-config.js
@@ -70,6 +70,5 @@ module.exports = {
         purgeOnly: ["/all.scss"], // applies purging only on the bulma css file
       },
     }, // must be after other CSS plugins
-    "gatsby-plugin-netlify", // make sure to keep it last in the array
   ],
 };
